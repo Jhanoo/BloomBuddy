36d6a40ec30b769a32fba9a189b81ab3530038af
Jhanoo <jhanoo@naver.com>
2022-07-07 21:41:23 +0900

    Apply .gitignore

--- app/src/main/java/com/example/bloombuddy/MainActivity.java
+++ app/src/main/java/com/example/bloombuddy/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.bloombuddy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.kakao.sdk.common.util.Utility;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String keyHash = Utility.INSTANCE.getKeyHash(this);
+        Log.d("keyhash", keyHash);
     }
 }
