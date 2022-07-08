9ce90fa0e34ff19dec9550e632b00b0f7de23c41
Jhanoo <jhanoo@naver.com>
2022-07-08 18:02:37 +0900

    kakao login 완성

--- app/src/main/java/com/example/bloombuddy/GlobalApplication.java
+++ app/src/main/java/com/example/bloombuddy/GlobalApplication.java
@@ -1,6 +1,5 @@
 package com.example.bloombuddy;
 
-
 import android.app.Application;
 
 import com.kakao.sdk.common.KakaoSdk;
@@ -15,7 +14,7 @@ public class GlobalApplication extends Application {
 
         // 네이티브 앱 키로 초기화
         KakaoSdk.init(this, getString(R.string.kakao_native_key));
-    }
 
+    }
 
 }
--- app/src/main/java/com/example/bloombuddy/MainActivity.java
+++ app/src/main/java/com/example/bloombuddy/MainActivity.java
@@ -5,13 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.kakao.sdk.auth.AuthApiClient;
 import com.kakao.sdk.common.util.Utility;
 import com.kakao.sdk.user.UserApiClient;
 import com.kakao.sdk.user.model.Account;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView kakao_nameTv;
+    private ImageView kakao_profileImgView;
+    private Button kakao_logoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,24 +30,57 @@ public class MainActivity extends AppCompatActivity {
         String keyHash = Utility.INSTANCE.getKeyHash(this);
         Log.d("keyhash", keyHash);
 
-        ImageButton kakao_login_button = (ImageButton)findViewById(R.id.kakao_login_button);
-        kakao_login_button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                if(UserApiClient.getInstance().isKakaoTalkLoginAvailable(MainActivity.this)){
-                    login();
+        ImageButton kakao_login_button = (ImageButton) findViewById(R.id.kakao_login_button);
+        kakao_profileImgView = (ImageView) findViewById(R.id.kakao_profileImgView);
+        kakao_nameTv = (TextView) findViewById(R.id.kakao_nameTv);
+        kakao_logoutBtn = findViewById(R.id.kakao_logoutBtn);
+
+        if (AuthApiClient.getInstance().hasToken()) {
+            UserApiClient.getInstance().accessTokenInfo((accessTokenInfo, error) -> {
+                if(error != null){
+                    Log.d("token error", "토큰 없음");
+                }
+                else if (accessTokenInfo != null) {
+                    Log.i("toekn ok", "토큰 정보 보기 성공" +
+                            "\n회원번호: ${tokenInfo.id}" +
+                            "\n만료시간: ${tokenInfo.expiresIn} 초");
                 }
                 else{
+                    Log.d("token ok", "토큰 있음");
+                }
+                return null;
+            });
+        } else {
+
+        }
+
+        kakao_login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (UserApiClient.getInstance().isKakaoTalkLoginAvailable(MainActivity.this)) {
+                    login();
+                } else {
                     accountLogin();
                 }
             }
         });
 
+        kakao_logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UserApiClient.getInstance().logout(error -> {
+                    return null;
+                });
+                kakao_nameTv.setText("logout");
+
+            }
+        });
+
     }
 
-    public void login(){
+    public void login() {
         String TAG = "login()";
-        UserApiClient.getInstance().loginWithKakaoTalk(MainActivity.this,(oAuthToken, error) -> {
+        UserApiClient.getInstance().loginWithKakaoTalk(MainActivity.this, (oAuthToken, error) -> {
             if (error != null) {
                 Log.e(TAG, "로그인 실패", error);
             } else if (oAuthToken != null) {
@@ -49,9 +91,9 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void accountLogin(){
+    public void accountLogin() {
         String TAG = "accountLogin()";
-        UserApiClient.getInstance().loginWithKakaoAccount(MainActivity.this,(oAuthToken, error) -> {
+        UserApiClient.getInstance().loginWithKakaoAccount(MainActivity.this, (oAuthToken, error) -> {
             if (error != null) {
                 Log.e(TAG, "로그인 실패", error);
             } else if (oAuthToken != null) {
@@ -62,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void getUserInfo(){
+    public void getUserInfo() {
         String TAG = "getUserInfo()";
         UserApiClient.getInstance().me((user, meError) -> {
             if (meError != null) {
@@ -72,14 +114,21 @@ public class MainActivity extends AppCompatActivity {
                 Log.i(TAG, user.toString());
                 {
                     Log.i(TAG, "사용자 정보 요청 성공" +
-                            "\n회원번호: "+user.getId() +
-                            "\n이메일: "+user.getKakaoAccount().getEmail());
+                            "\n회원번호: " + user.getId());
                 }
                 Account user1 = user.getKakaoAccount();
-                System.out.println("사용자 계정" + user1);
+                {
+                    Log.i(TAG, "닉네임: " + user1.getName() +
+                            "\n프로필: " + user1.getProfile());
+                }
+                Glide.with(this)
+                        .load(user1.getProfile().getProfileImageUrl())
+                        .into(kakao_profileImgView);
+                kakao_nameTv.setText(user1.getProfile().getNickname());
             }
             return null;
         });
     }
+
 
 }
